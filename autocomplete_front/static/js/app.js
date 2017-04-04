178d2c6a1d6c26b865ee19c3733b5f81f399e869
Victor Quach <varaal@gmail.com>
2017-04-05 01:09:32 +0200

    Display textarea for easy export

--- autocomplete_front/static/js/app.js
+++ autocomplete_front/static/js/app.js
@@ -3,6 +3,25 @@ Vue.component('person', {
     props: ['person']
 });
 
+Vue.component('results', {
+    template: '#results',
+    props: ['results'],
+    computed: {
+        output: function() {
+            vm = this;
+            if (vm.results.length == 0) {
+                return "";
+            } else {
+                return vm.results.map(function(item) {
+                        return item.mail
+                    }).reduce(function(a,b) {
+                        return b + ";\n" + a
+                    });
+            }
+        }
+    }
+});
+
 var app = new Vue({
     el: '#app',
     components: {
@@ -11,15 +30,14 @@ var app = new Vue({
     data: function() {
         return {
             ldapTemplate: '<div class="ldap-item"><div class="img-container"><img height="36px" :src="item.photo"></div><p><strong>{{item.firstname}} {{item.lastname}} ({{item.promo}})</strong> <br/> {{item.mail}}</p> </div>',
-            current: null
+            current: null,
+            results: []
         }
     },
     methods: {
-        ldapCallback: function(item, targetVM) {
-            const that = targetVM;
-            that.reset();
-            that.value = item.mail;
+        ldapCallback: function(item) {
             this.current = item;
+            this.results.push(item)
         }
     }
 });
