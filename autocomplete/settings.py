72d53697d9706d8c6e383abd78b8501f800c39e4
Victor Quach <varaal@gmail.com>
2017-04-04 22:28:12 +0200

    Fix typo in settings file

--- autocomplete/settings.py
+++ autocomplete/settings.py
@@ -134,5 +134,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "assets/static/")
