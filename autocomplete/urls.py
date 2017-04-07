babfcc81fe4ef8dc53d4195f949efd104822683b
Victor Quach <varaal@gmail.com>
2017-04-07 03:41:52 +0200

    Change name of view from index to search

--- autocomplete/urls.py
+++ autocomplete/urls.py
@@ -17,12 +17,12 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from ldap_back.views import ldap_search
-from autocomplete_front.views import index
+from autocomplete_front.views import search
 from django_cas_ng.views import login, logout
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', index, name="index"),
+    url(r'^$', search, name="search"),
     url(r'^ldap_search$', ldap_search, name="ldap_search"),
     url(r'^login$', login, name='cas_ng_login'),
     url(r'^logout$', logout, name='cas_ng_logout'),
--- autocomplete_front/views.py
+++ autocomplete_front/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
-def index(request):
-    return render(request, "autocomplete_front/index.html")
+
+def search(request):
+    return render(request, "autocomplete_front/search.html")
